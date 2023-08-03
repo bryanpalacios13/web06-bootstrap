1fdfd5d4114c4ba7c166e29f18d5a699f3b06b24
bryanpalacios13 <bryan.axel1301@gmail.com>
2023-08-03 17:24:22 -0600

    feat: juego de colores

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ console.log("Página en construcción con Patricio Estrella");
 function clickPatricio() {
     alert ("La mayonesa es un instrumento? xd");
 }
+
+function changeColor( elementHTML, color){
+    alert(" Que emocion, voy a cambiar mi color");
+    console.log(elementHTML);
+    console.log(color);
+
+    elementHTML.style.color = color;
+}
