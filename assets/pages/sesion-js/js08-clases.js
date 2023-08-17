027bbef3c1a510ace595254ac7c6bdbe291b2e7e
bryanpalacios13 <bryan.axel1301@gmail.com>
2023-08-17 17:10:40 -0600

    ci: llamar productos API

--- assets/pages/sesion-js/js08-clases.js
+++ assets/pages/sesion-js/js08-clases.js
@@ -7,6 +7,7 @@ const getProducts = async () => {
     const responseJSON = await fetch(url);
     const response = await responseJSON.json();
     console.log(response);
+    return response;
 };
 
 /**
@@ -14,21 +15,99 @@ const getProducts = async () => {
  * El nombre de las clases se realizar con UpperCamelCase
 */
 
-class Products{
+class Products {
     // definimos atributos
 
     // El método constructor nos ayuda a instanciar un objeto 
-    constructor( id, name ){
+    constructor(id, name) {
         this.name = name; // creando el atributo name y le asignamos el valor de parámetro name 
         this.id = id;
         this.createAt = new Date().getTime();
-        console.log(`Usuario ${this.name} se creó el ${new Date().toLocaleString}`);
+        // console.log(`Producto ${this.name} se creó el ${new Date().toLocaleString()}`);
+    }
+
+    lifeSpan() {
+        return new Date().getTime() - this.createAt;
     }
 }
 
-function createProductsOfClassProducts (){
-            // instanciar la clase products para crear un objeto
-    const zote = new Products();
+function createProductsOfClassProducts() {
+    // instanciar la clase products para crear un objeto
+    const zote = new Products(1, "zote");
+    const products = [];
+    products.push(zote);
+    products.push(new Products(2, "Palmolive"));
+    products.push(new Products(3, "Coca Cola 2L"));
+    products.push(new Products(4, "Cajeta Corona"));
+    products.push(new Products(5, "Chips Moradas"));
+    products.push(new Products(6, "Salsa Valentina"));
+    products.push(new Products(7, "Sopa Nissin"));
+
+    // console.table (products);
+    return products;
 }
 
-createProductsOfClassProducts();
+function lifeSpanProducts(products) {
+    // El método map sobre un arreglo, intera sobre cada elemento 
+    // de arreglo y entrega un nuevo arreglo
+    const productsLifeSpan = products.map(product =>
+        `<li> ${product.name} se creo hace ${product.lifeSpan() / 1000} </li>`
+    );
+    return productsLifeSpan;
+}
+
+function insertListItems(listItems) {
+    const products = document.getElementById("products");
+    const unorderList = `<ul> ${listItems.join("")} </ul>`;
+
+    products.innerHTML = unorderList;
+}
+
+function onclickLifeSpan() {
+    const products = createProductsOfClassProducts(); // Crea los objetos
+    setTimeout(() => {
+        const productListItem = lifeSpanProducts(products); // 
+        insertListItems(productListItem);
+    }, 5000);
+}
+
+// ==============================================================================
+
+async function showProducts() {
+    // const products = createProductsOfClassProducts();
+    const products = await createProductsOfFakeStore();
+    const productsCards = createCardsOfProducts(products);
+    insertCards(productsCards);
+}
+
+async function createProductsOfFakeStore(){
+    const fakeProducts = await getProducts();
+
+    // return fakeProducts.maps((fakeProduct) => new Products ( fakeProduct.id, fakeProduct.title ) );
+    return fakeProducts.map(({id, title}) => new Products(id, title) );
+}
+
+function createCardsOfProducts(products) {
+
+    return products.map((product) =>{
+        return`
+        <div class="card col-4 m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${product.id}</h6>
+                <p class="card-text">Some title content pon uwu xd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+        `
+    })
+
+}
+
+function insertCards( productCards ){
+    const products = document.getElementById("products");
+    const cards = `<div class="row"> ${productCards.join("")} </div>`;
+
+    products.innerHTML = cards;
+}
